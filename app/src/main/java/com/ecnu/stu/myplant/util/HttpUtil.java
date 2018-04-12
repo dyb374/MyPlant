d2fa7a65542aca9a3281d8c131098d8ccc3f02a7
Andrew <ybdong374@126.com>
2018-04-12 21:50:30 +0800

    处理天气数据

---
+++ app/src/main/java/com/ecnu/stu/myplant/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package com.ecnu.stu.myplant.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Andrew Dong on 2018/4/12.
+ * 和服务器进行交互
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest (String address, okhttp3.Callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue();
+    }
+}
