4a21efb74188c2a6b7dd8687ca9f3bd534523d6e
Andrew <ybdong374@126.com>
2018-04-13 14:11:32 +0800

    定位服务

--- app/src/main/java/com/ecnu/stu/myplant/util/HttpUtil.java
+++ app/src/main/java/com/ecnu/stu/myplant/util/HttpUtil.java
@@ -1,5 +1,6 @@
 package com.ecnu.stu.myplant.util;
 
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
@@ -10,9 +11,9 @@ import okhttp3.Request;
 
 public class HttpUtil {
 
-    public static void sendOkHttpRequest (String address, okhttp3.Callback) {
+    public static void sendOkHttpRequest (String address, Callback callback) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(address).build();
-        client.newCall(request).enqueue();
+        client.newCall(request).enqueue(callback);
     }
 }
