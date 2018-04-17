bb7fd7a33df33276733ef9a17fd30d6813f57177
Andrew <ybdong374@126.com>
2018-04-18 00:06:35 +0800

    地图

--- app/src/main/java/com/ecnu/myplant/MainActivity.java
+++ app/src/main/java/com/ecnu/myplant/MainActivity.java
@@ -1,18 +1,22 @@
 package com.ecnu.myplant;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
+import com.ecnu.myplant.gson.Weather;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -54,6 +58,16 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 requestLocation();
             }
+
+            Button button = (Button) findViewById(R.id.button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                    startActivity(intent);
+                }
+            });
+
 
             flag = true;
         } else {
--- app/src/main/java/com/ecnu/myplant/gson/Weather.java
+++ app/src/main/java/com/ecnu/myplant/gson/Weather.java
@@ -2,7 +2,9 @@ package com.ecnu.myplant.gson;
 
 /**
  * Created by Andrew Dong on 2018/4/13.
+ * 这个类是关键
  * 引用各个gson实体类
+ * 到时候需要通过这个类创建天气的实体
  */
 
 public class Weather {
